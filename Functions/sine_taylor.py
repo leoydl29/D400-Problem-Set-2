a37aac2dbd6e691a40bd3b86b5deae22b5b36376
Leo L <l.y.l0903@outlook.com>
2025-10-30 12:16:34 +0000

    Added Comments explaining the function to the sine_taylor function

--- Functions/sine_taylor.py
+++ Functions/sine_taylor.py
@@ -2,9 +2,21 @@ import math
 
 def taylor_sine(order, radians):
 
+    """
+    This function approximates sine by using the taylor expansion. 
+
+    Param: 
+        order(int): This parameter determines the order of the expansion for the taylor series 
+        radians(float): This parameter determines the x value in the taylor expansion
+    
+    Output: 
+        expansion(float): This is where the output of the taylor expansion is stored
+    
+    """
     expansion = 0
 
     for n in range(order):
+        
         exponent = 2 * n+ 1
         
         x_power = radians ** exponent
